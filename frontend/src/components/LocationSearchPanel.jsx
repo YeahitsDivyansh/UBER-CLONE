385ed7e4ba6f26672f0f0c3b79efdb98ddb9ead4
YeahitsDivyansh <divyansh972003@gmail.com>
2025-03-18 11:24:54 +0530

    location pannel creation

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,5 +1,16 @@
 const LocationSearchPanel = () => {
-  return <div>LocationSearchPanel</div>;
+  return (
+    <div>
+      <div className="flex gap-4 items-center my-4 justify-start">
+        <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full">
+          <i className="ri-map-pin-fill"></i>
+        </h2>
+        <h4 className="font-medium">
+          House No.43, Near SDSN Degree College, Gomtinagar, Lucknow
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default LocationSearchPanel;
