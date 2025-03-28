c2d4e246599a143c0b125dc67d6865d578b61bff
YeahitsDivyansh <divyansh972003@gmail.com>
2025-03-28 12:13:13 +0530

    enhance captain home page layout and update wrapper component naming

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,5 +1,64 @@
+import { Link } from "react-router-dom";
+
 const CaptainHome = () => {
-  return <div>Captain Home</div>;
+  return (
+    <div className="h-screen">
+      <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
+        <img
+          className="w-16"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <Link
+          to="/captain-home"
+          className=" h-10 w-10 bg-white flex items-center justify-center rounded-full"
+        >
+          <i className="text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+      <div className="h-3/5">
+        <img
+          className="h-full w-full object-cover"
+          src="https://s.wsj.net/public/resources/images/BN-XR452_201802_M_20180228165525.gif"
+        />
+      </div>
+
+      <div className="h-2/5 p-6">
+        <div className="flex items-center justify-between">
+          <div className="items-center justify-start flex gap-3">
+            <img
+              className="h-10 w-10 rounded-full object-cover"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlMd7stpWUCmjpfRjUsQ72xSWikidbgaI1w&s"
+              alt=""
+            />
+            <h4 className="text-lg font-medium">Harsh Patel</h4>
+          </div>
+          <div>
+            <h4 className="text-xl font-semibold">Rs-295.20</h4>
+            <p className="text-sm text-gray-600">Earned</p>
+          </div>
+        </div>
+
+        <div className="flex mt-6 p-3 bg-gray-100 rounded-xl justify-centre gap-4 items-start">
+          <div className="text-center">
+            <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
+            <h5 className="text-lg font-medium">10.2</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+          <div className="text-center">
+            <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+            <h5 className="text-lg font-medium">10.2</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+          <div className="text-center">
+            <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+            <h5 className="text-lg font-medium">10.2</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CaptainHome;
--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { CaptainDataContext } from "../context/CaptainContext";
 import axios from "axios";
 
-const UserProtectWrapper = ({ children }) => {
+const CaptainProtectWrapper = ({ children }) => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   const { captain, setCaptain } = useContext(CaptainDataContext);
@@ -15,25 +15,25 @@ const UserProtectWrapper = ({ children }) => {
     if (!token) {
       navigate("/captain-login");
     }
-  }, [token]);
 
-  axios
-    .get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => {
-      if (response.status === 200) {
-        setCaptain(response.data.captain);
-        setIsLoading(false);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      localStorage.removeItem("token");
-      navigate("/captain-login");
-    });
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setCaptain(response.data.captain);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.removeItem("token");
+        navigate("/captain-login");
+      });
+  }, [token]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -41,4 +41,4 @@ const UserProtectWrapper = ({ children }) => {
 
   return <>{children}</>;
 };
-export default UserProtectWrapper;
+export default CaptainProtectWrapper;
